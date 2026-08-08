511574f448d561c9edfd24357e3d0e7d6d42f8e2
Malcom3D <Malcom3D@users.noreply.github.com>
2026-08-08 16:58:05 +0200

    release(all): pbrAudio 0.2.4.1 beta

--- lib/functions.py
+++ lib/functions.py
@@ -563,11 +563,13 @@ def _abs_start_frame(config_obj: Any):
 
 def _adjust_for_fracture_shard(start_samples, stop_samples, sample_rate, sfps, config_obj1, config_obj2):
     """Adjust sample range for fracture and shard events."""
-    abs_start_frame1 = _abs_start_frame(config_obj1)
-    abs_start_frame2 = _abs_start_frame(config_obj2)
-    abs_start_frame = abs_start_frame1 if not abs_start_frame1 == 0 else abs_start_frame2
+    if config_obj1.fractured == False and config_obj2.fractured == False and config_obj1.is_shard == False and config_obj2.is_shard == False:
+        return start_samples, stop_samples
 
-    fracture_samples1 = -1
+    # ToDo: handle static-static to static-shards?
+    abs_start_frame = max(_abs_start_frame(config_obj1), _abs_start_frame(config_obj2))
+
+    fracture_samples1 = stop_samples
     if not config_obj1.fractured == False:
         fracture_samples = (config_obj1.fractured - abs_start_frame) * sample_rate / sfps
         if stop_samples >= fracture_samples >= start_samples:
@@ -575,7 +577,7 @@ def _adjust_for_fracture_shard(start_samples, stop_samples, sample_rate, sfps, c
         elif start_samples > fracture_samples:
             fracture_samples1 = start_samples
 
-    fracture_samples2 = -1
+    fracture_samples2 = stop_samples
     if not config_obj2.fractured == False:
         fracture_samples = (config_obj2.fractured - abs_start_frame) * sample_rate / sfps
         if stop_samples >= fracture_samples >= start_samples:
@@ -583,7 +585,7 @@ def _adjust_for_fracture_shard(start_samples, stop_samples, sample_rate, sfps, c
         elif start_samples < fracture_samples:
             fracture_samples2 = start_samples
 
-    shard_samples1 = -1
+    shard_samples1 = start_samples
     if not config_obj1.is_shard == False:
         shard_samples = (config_obj1.is_shard - abs_start_frame) * sample_rate / sfps
         if stop_samples >= shard_samples >= start_samples:
@@ -591,7 +593,7 @@ def _adjust_for_fracture_shard(start_samples, stop_samples, sample_rate, sfps, c
         elif stop_samples < shard_samples:
             shard_samples1 = stop_samples
 
-    shard_samples2 = -1
+    shard_samples2 = start_samples
     if not config_obj2.is_shard == False:
         shard_samples = (config_obj2.is_shard - abs_start_frame) * sample_rate / sfps
         if stop_samples >= shard_samples >= start_samples:
